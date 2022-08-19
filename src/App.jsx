c80ca10eb0d40d5a292301b58b0e9a34af836d05
CodingCocho <raulujr@gmail.com>
2022-08-19 10:38:29 -0700

    Finished product and added comments for deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css';
 import { AdviceGenerator } from './components/AdviceGenerator';
 
 function App() {
+
+  document.body.classList.add('body-styling')
+
   return (
     <div className="App">
       <AdviceGenerator />
--- src/components/AdviceGenerator.jsx
+++ src/components/AdviceGenerator.jsx
@@ -1,21 +1,40 @@
-import './styles/AdviceGenerator.css'
-import { useState, useEffect } from 'react'
+import './styles/AdviceGenerator.css';
+import { useState } from 'react';
+import mobileDivider from '../images/pattern-divider-mobile.svg';
+import desktopDivider from '../images/pattern-divider-desktop.svg';
+import dice from '../images/icon-dice.svg';
 
 // Export our advice generator component
 export const AdviceGenerator = () =>
 {
+    // Hold the useStates for our component
+    const [adviceNumber, setAdviceNumber] = useState('ADVICE #139');
+    const [quote, setQuote] = useState('If you\'\re going bald, don\'\t comb your hair over your bald patch');
 
-    const [quote, setQuote] = useState('');
-
+    // API function that retrieves an advice slip object
     const fetchAQuote = () =>
     {
-
+        // Make fetch call to API
+        fetch('https://api.adviceslip.com/advice')
+            .then((promise) => promise.json())
+            .then((data) => 
+            {
+               // Hold advice object 
+               let adviceObject = data.slip;
+               // Hold the new strings for our useStates    
+               let adviceString = 'ADVICE #' + adviceObject.id;
+               let quoteString = adviceObject.advice;
+               // Update our component's useStates   
+               setAdviceNumber(adviceString);
+               setQuote(quoteString);
+            })
+            // Catch an error
+            .catch((err) =>
+            {
+                // Print out the error to see what is wrong
+                console.log(err);
+            })
     }
-
-    useEffect(() =>
-    {
-
-    }, [])
 
     return (
         // Create container for component
@@ -23,33 +42,35 @@ export const AdviceGenerator = () =>
             {/* Hold advice id */}
             <p 
             className="advice-number">
-                Advice #420
+                {adviceNumber}
             </p>
             {/* Hold advice quote */}
             <p 
             className="quote">
-                {quote}
+                "{quote}"
             </p>
             {/* Hold container divider image for mobile viewport */}
             <img 
             alt="container-divider-mobile"
             className="divider-image-mobile"
-            src="../images/pattern-divider-mobile.svg"  
+            src={mobileDivider} 
             />
             {/* Hold container divider image for desktop viewport */}
             <img 
             alt="container-divider-desktop"
             className="divider-image-desktop" 
-            src="../images/pattern-divider-desktop.svg"
+            src={desktopDivider}
             />
             {/* Create container for api fetch button */}
             <div 
-            className="api-button">
+            className="api-button"
+            onClick={fetchAQuote}
+            >
                 {/* Image for api fetch button */}
                 <img  
                 alt="api-dice"
                 className="dice"
-                src="../images/icon-dice.svg" 
+                src={dice}
                 />
             </div>
         </div>
